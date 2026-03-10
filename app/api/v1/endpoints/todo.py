746f967ebc2e9d3eaf412cb746d088d365ba6db9
vishruththatiparthi <vishruth.thatiparthi@nstarxinc.com>
2026-03-10 15:58:49 +0530

    JWT auth

--- app/api/v1/endpoints/todo.py
+++ app/api/v1/endpoints/todo.py
@@ -5,11 +5,19 @@ from app.db.database import get_db
 from app.db import models
 from app.schemas import todo_schema
 
+# JWT + Authorization imports
+from app.api.dependencies.jwt_auth import get_current_user
+from app.api.dependencies.authorization import require_role
+
 router = APIRouter()
 
 
 @router.post("/todos", response_model=todo_schema.TodoResponse)
-def create_todo(todo: todo_schema.TodoCreate, db: Session = Depends(get_db)):
+def create_todo(
+    todo: todo_schema.TodoCreate,
+    db: Session = Depends(get_db),
+    user=Depends(get_current_user)   # requires valid JWT
+):
     new_todo = models.Todo(task=todo.task)
 
     db.add(new_todo)
@@ -20,12 +28,19 @@ def create_todo(todo: todo_schema.TodoCreate, db: Session = Depends(get_db)):
 
 
 @router.get("/todos", response_model=list[todo_schema.TodoResponse])
-def get_todos(db: Session = Depends(get_db)):
+def get_todos(
+    db: Session = Depends(get_db),
+    user=Depends(get_current_user)   # requires valid JWT
+):
     return db.query(models.Todo).all()
 
 
 @router.get("/todos/{todo_id}", response_model=todo_schema.TodoResponse)
-def get_todo(todo_id: int, db: Session = Depends(get_db)):
+def get_todo(
+    todo_id: int,
+    db: Session = Depends(get_db),
+    user=Depends(get_current_user)   # requires valid JWT
+):
     todo = db.query(models.Todo).filter(models.Todo.id == todo_id).first()
 
     if not todo:
@@ -35,7 +50,12 @@ def get_todo(todo_id: int, db: Session = Depends(get_db)):
 
 
 @router.put("/todos/{todo_id}", response_model=todo_schema.TodoResponse)
-def update_todo(todo_id: int, updated: todo_schema.TodoCreate, db: Session = Depends(get_db)):
+def update_todo(
+    todo_id: int,
+    updated: todo_schema.TodoCreate,
+    db: Session = Depends(get_db),
+    user=Depends(get_current_user)   # requires valid JWT
+):
     todo = db.query(models.Todo).filter(models.Todo.id == todo_id).first()
 
     if not todo:
@@ -50,7 +70,11 @@ def update_todo(todo_id: int, updated: todo_schema.TodoCreate, db: Session = Dep
 
 
 @router.delete("/todos/{todo_id}")
-def delete_todo(todo_id: int, db: Session = Depends(get_db)):
+def delete_todo(
+    todo_id: int,
+    db: Session = Depends(get_db),
+    user=Depends(require_role("admin"))   # only admin can delete
+):
     todo = db.query(models.Todo).filter(models.Todo.id == todo_id).first()
 
     if not todo:
